e40176bdd7ebd1251abd6b30760549169db49bd6
Alexey Loktev <a.a.loktev@yandex.ru>
2022-06-02 12:59:49 +0300

    Выполнена 1 часть 6 задания

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const guessBot = function () {
+  let randomNumber = Math.round(Math.random() * 101);
+  return function () {
+    let userNumber = prompt("Угадай число от 0 до 100!");
+    if (userNumber === null) {
+      alert("Игра окончена!");
+    } else if (isNaN(userNumber)) {
+      alert("Введи число!");
+      play();
+    } else if (+userNumber > randomNumber) {
+      alert("Загаданное число меньше!");
+      play();
+    } else if (+userNumber < randomNumber) {
+      alert("Загаданное число больше!");
+      play();
+    } else if (+userNumber === randomNumber) {
+      alert("Поздравляю, Вы угадали!!!");
+    }
+  };
+};
+
+const play = guessBot();
+
+const goPlay = function () {
+  let quest = confirm("Хочешь поиграть?!");
+  if (quest === true) {
+    play();
+  } else alert("До свидания!");
+};
+
+goPlay();
